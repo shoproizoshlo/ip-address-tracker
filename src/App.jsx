65a22f51712047570999d2c2ca45f9aaf80536ad
shoproizoshlo <iamsusunny@gmail.com>
2024-01-09 12:41:06 +0100

    set up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Arrow from "./assets/icon-arrow.svg";
+import Info from "./components/Info";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className="background"></div>
+      <div className="map"></div>
+      <h1>IP Address tracker </h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input type="text" />
+        <button>
+          <img src={Arrow} alt="Arrow" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Info />
+      <Info />
+      <Info />
+      <Info />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+const Info = () => {
+  return (
+    <div>
+      <p>some text</p>
+      <p>another bigger text</p>
+    </div>
+  );
+};
+
+export default Info;
