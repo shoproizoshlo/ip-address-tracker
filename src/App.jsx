c5502bb40fa609723bf9597479e32f22a13ecf13
shoproizoshlo <iamsusunny@gmail.com>
2024-01-10 11:54:41 +0100

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Arrow from "./assets/icon-arrow.svg";
+import BgDesktop from "./assets/pattern-bg-desktop.png";
+import BgMobile from "./assets/pattern-bg-mobile.png";
 import Info from "./components/Info";
 import MyMap from "./components/MyMap";
 import "./App.css";
@@ -25,36 +27,46 @@ function App() {
 
   return (
     <>
-      <div className="background"></div>
+      <div className="background">
+        <picture>
+          <source
+            srcSet={BgMobile}
+            type="image/jpg"
+            media="(max-width:992px)"
+          />
+          <img src={BgDesktop} alt="" />
+        </picture>
+      </div>
       <div className="map" id="map">
         {data && (
           <>
-            <h1>Your Map</h1>
             <MyMap lat={data.location.lat} lng={data.location.lng} />
           </>
         )}
       </div>
-      <h1>IP Address tracker </h1>
-      <div>
-        <input type="text" />
-        <button>
-          <img src={Arrow} alt="Arrow" />
-        </button>
-      </div>
+      <div className="info">
+        <h1>IP Address tracker </h1>
+        <div>
+          <input type="text" />
+          <button>
+            <img src={Arrow} alt="Arrow" />
+          </button>
+        </div>
 
-      <div>
-        {data && (
-          <>
-            <Info heading="ip" dataInfo={data.ip} />
-            <Info
-              heading="location"
-              dataInfo={`${data.location.region}, ${data.location.country}`}
-            />
-            <Info heading="timezone" dataInfo={data.location.timezone} />
-            <Info heading="isp" dataInfo={data.isp} />
-            <Info heading="lat" dataInfo={data.location.lat} />
-          </>
-        )}
+        <div>
+          {data && (
+            <>
+              <Info heading="ip" dataInfo={data.ip} />
+              <Info
+                heading="location"
+                dataInfo={`${data.location.region}, ${data.location.country}`}
+              />
+              <Info heading="timezone" dataInfo={data.location.timezone} />
+              <Info heading="isp" dataInfo={data.isp} />
+              <Info heading="lat" dataInfo={data.location.lat} />
+            </>
+          )}
+        </div>
       </div>
     </>
   );
